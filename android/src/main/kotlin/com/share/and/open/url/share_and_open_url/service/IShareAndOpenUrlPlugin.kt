4fc2f9f92539f4e6e34999a82a9cc9e73dabdb86
Bedirhan Saglam <bedirhansaglam270@gmail.com>
2024-04-29 16:58:36 +0300

    ⚡️ update native codes

---
+++ android/src/main/kotlin/com/share/and/open/url/share_and_open_url/service/IShareAndOpenUrlPlugin.kt
@@ -0,0 +1,6 @@
+package com.share.and.open.url.share_and_open_url.service
+
+interface ShareAndOpenUrlPluginInterface {
+    fun shareText(text: String)
+    fun openUrl(url: String)
+}
---
+++ android/src/main/kotlin/com/share/and/open/url/share_and_open_url/service/ShareAndOpenUrlPluginService.kt
@@ -0,0 +1,4 @@
+package com.share.and.open.url.share_and_open_url.service
+
+class ShareAndOpenUrlPluginService {
+}
